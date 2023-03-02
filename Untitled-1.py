7aaedaf0efc1d1cf5bfab940a1a4a9e1cb06c5ae
Prayati Paliwal <prayatipaliwal@gmail.com>
2023-03-02 15:07:47 +0530

    updated code

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,14 +4,27 @@ wind= Tk()
 wind.title("Calculator")
 wind.geometry("300x400")
 
+text  =Entry(wind, font=("calibri",16))
+text.pack(fill=X, padx=5,ipadx=5,ipady=5)
+
+def addToText(n):
+    text.insert(END,n)
+
 frame = Frame(wind)
 frame.pack(side=TOP, anchor=NW)
 
 rightFrame = Frame(frame)
-frame.pack(side=RIGHT)
+rightFrame.pack(side=RIGHT)
 
+frame1 =Frame(frame)
+frame1.pack()
 
-
-
+#btn1 to 3
+btn1 = Button(frame1,text="1",width=9,height=3, command=lambda:addToText("1"))
+btn1.pack(side=LEFT)
+btn2 = Button(frame1,text="2",width=9,height=3, command=lambda:addToText("2"))
+btn2.pack(side=LEFT)
+btn3 = Button(frame1,text="3",width=9,height=3, command=lambda:addToText("3"))
+btn3.pack(side=LEFT)
 
 wind.mainloop()
